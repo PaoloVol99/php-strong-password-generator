b2bdc821e1a127499a0b2e1b369d6afd53a873c0
“PaoloVol99” <paolo.volpini99@gmail.com>
2023-03-21 17:23:48 +0100

    FEAT Milestone 2

--- index.php
+++ index.php
@@ -1,19 +1,6 @@
 <?php
-$password_source = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-
-$password_length = $_GET['password_length'] ?? false;
-
-$password = '';
-
-for ($i = 0; $i < $password_length; $i++) {
-    $random_index = rand(0 , (strlen($password_source) - 1));
-    $password = $password . $password_source[$random_index];
-
-    // var_dump($random_index, $password);
-}
-
+include __DIR__ . "/functions.php";
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
